9f34d89a9e6e75f4adf5aaa8c2ca354d40b1fb3c
SayemImran <ik262142@gmail.com>
2026-06-19 19:44:59 +0600

    RBAC sidebar links added

---
+++ src/app/dashboard/seller/orders/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Orders = () => {
+    return (
+        <div>
+            This is order management page
+        </div>
+    );
+};
+
+export default Orders;
--- src/components/dashboard/Sidebar.jsx
+++ src/components/dashboard/Sidebar.jsx
@@ -10,40 +10,38 @@ import {
 } from "@gravity-ui/icons";
 import { Button, Drawer } from "@heroui/react";
 import { headers } from "next/headers";
+import Link from "next/link";
 
 export async function Sidebar() {
   const sidebaritemsbyrole = {
     Seller: [
-      { icon: House, label: "Home", links: "dashboard/seller" },
-      { icon: House, label: "Profile", links: "dashboard/seller" },
-      { icon: House, label: "My Listings", links: "dashboard/seller" },
-      { icon: House, label: "Add product", links: "dashboard/seller" },
+      { icon: House, label: "Overview", links: "/dashboard/seller" },
+      { icon: House, label: "Add Product", links: "/dashboard/seller/add-product" },
+      { icon: House, label: "My Product", links: "/dashboard/seller/products" },
+      { icon: House, label: "Manage Orders", links: "/dashboard/seller/orders" },
+      { icon: House, label: "Sales Analytics", links: "/dashboard/seller/analytics" },
     ],
     Buyer: [
-      { icon: House, label: "Home", links: "dashboard/buyer" },
-      { icon: House, label: "My orders", links: "dashboard/buyer" },
-      { icon: House, label: "Wishlist", links: "dashboard/buyer" },
-      { icon: House, label: "Profile", links: "dashboard/buyer" },
+      { icon: House, label: "Overview", links: "/dashboard/buyer/" },
+      { icon: House, label: "My orders", links: "/dashboard/buyer/" },
+      { icon: House, label: "Wishlist", links: "/dashboard/buyer/" },
+      { icon: House, label: "Payment History", links: "/dashboard/buyer/" },
     ],
     Admin: [
-      { icon: House, label: "Home", links: "dashboard/admin" },
-      { icon: House, label: "Users", links: "dashboard/admin" },
-      { icon: House, label: "Orders", links: "dashboard/admin" },
-      { icon: House, label: "Analytics", links: "dashboard/admin" },
-      { icon: House, label: "Statistics", links: "dashboard/admin" },
-      { icon: House, label: "Profile", links: "dashboard/admin" },
+      { icon: House, label: "Overview", links: "/dashboard/admin" },
+      { icon: House, label: "Manage User", links: "/dashboard/admin/" },
+      { icon: House, label: "Manage Prooducts", links: "/dashboard/admin/" },
+      { icon: House, label: "Manage Orders", links: "/dashboard/admin/" },
+      { icon: House, label: "Analytics", links: "/dashboard/admin/" },
     ],
   };
 
-  
   const session = await auth.api.getSession({
     headers: await headers(),
   });
   const user = session?.user;
-  console.log(user);
   const role = user?.role || "buyer";
   const navItems = sidebaritemsbyrole[role];
-console.log(navItems);
 
   return (
     <Drawer>
@@ -53,14 +51,16 @@ console.log(navItems);
       </Button>
       <nav className="flex flex-col gap-1">
         {navItems.map((item) => (
-          <button
-            key={item.label}
-            className="flex items-center gap-3 rounded-xl px-3 py-2.5 text-sm text-foreground transition-colors hover:bg-default"
-            type="button"
-          >
-            <item.icon className="size-5 text-muted" />
-            {item.label}
-          </button>
+          <Link href={item.links} key={item.label}>
+            <button
+              
+              className="flex items-center gap-3 rounded-xl px-3 py-2.5 text-sm text-foreground transition-colors hover:bg-default"
+              type="button"
+            >
+              <item.icon className="size-5 text-muted" />
+              {item.label}
+            </button>
+          </Link>
         ))}
       </nav>
 
